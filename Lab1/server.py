ac8d49beeca2e37ebd084846905fc972b49b0c10
Gustavo de Souza Silva <xgustavossilva@gmail.com>
2017-04-05 12:11:31 -0300

    Adicionado sistema de envio de mensagens

--- Lab1/server.py
+++ Lab1/server.py
@@ -2,12 +2,17 @@ import socket
 
 s = socket.socket()
 host = "localhost"
-port = 12345
+port = 12346
 s.bind((host,port))
-
 s.listen(5)
+
+c,addr = s.accept()
+print 'Got connection from', addr
+
 while True:
-	c,addr = s.accept()
-	print 'Got connection from', addr
-	c.send('Thank you for connection')
-	c.close()
+	print c.recv(1024)
+	str = raw_input("Digite sua resposta: ")
+	c.send(str)
+
+c.close()
+s.close()
