ab3ecdfc5041d290c6933897d717f1301ce5d603
Gustavo Silva <xgustavossilva@gmail.com>
2017-06-05 18:20:03 -0700

    Laboratorio 3 finalizado

--- Lab3/ExercicioLab3Receiver.java
+++ Lab3/ExercicioLab3Receiver.java
@@ -10,19 +10,35 @@ public class ExercicioLab3Receiver {
   public static void main(String[] args) {
     MulticastSocket socket = null;
     DatagramPacket inPacket = null;
+	DatagramPacket outPacket = null;
+	byte[] outBuf;
     byte[] inBuf = new byte[256];
+    final int PORT = 8887;
     Scanner leitura = new Scanner(System.in);
     try {
+	  String sendmsg;
       //Prepare to join multicast group
       socket = new MulticastSocket(8888);
       InetAddress address = InetAddress.getByName("224.2.2.3");
       socket.joinGroup(address);
  
       while (true) {
-        inPacket = new DatagramPacket(inBuf, inBuf.length);
-        socket.receive(inPacket);
-        String msg = new String(inBuf, 0, inPacket.getLength());
-        System.out.println("From " + inPacket.getAddress() + " Msg : " + msg);
+		    inPacket = new DatagramPacket(inBuf, inBuf.length);
+		    socket.receive(inPacket);
+		    String msg = new String(inBuf, 0, inPacket.getLength());
+		    System.out.println("From " + inPacket.getAddress() + " Msg : " + msg);
+			InetAddress addressSend = InetAddress.getByName("224.2.2.4");
+		  	System.out.print("Digite sua mensagem: ");
+		  	sendmsg = leitura.nextLine();
+		    outBuf = sendmsg.getBytes();
+		    outPacket = new DatagramPacket(outBuf, outBuf.length, addressSend, PORT);
+		    socket.send(outPacket);	
+
+        try {
+			Thread.sleep(500);
+        } catch (InterruptedException ie) {
+        }
+
       }
     } catch (IOException ioe) {
       System.out.println(ioe);
--- Lab3/ExercicioLab3Sender.java
+++ Lab3/ExercicioLab3Sender.java
@@ -7,30 +7,39 @@ import java.util.Scanner;
  */
 public class ExercicioLab3Sender {
   public static void main(String[] args) {
-    DatagramSocket socket = null;
+    MulticastSocket socket = null;
     DatagramPacket outPacket = null;
+    DatagramPacket inPacket = null;
+	byte[] inBuf = new byte[256];
     byte[] outBuf;
     final int PORT = 8888;
     Scanner leitura = new Scanner(System.in);
  
     try {	
-      socket = new DatagramSocket();
+      socket = new MulticastSocket(8887);
       String msg;
+	  String receivemsg;
+      InetAddress addressRec = InetAddress.getByName("224.2.2.4");
+	  socket.joinGroup(addressRec);
  
       while (true) {
-      	System.out.println("Digite sua mensagem");
+
+		//Envio de msg
+      	System.out.print("Digite sua mensagem: ");
       	msg = leitura.nextLine();
         outBuf = msg.getBytes();
- 
-        //Send to multicast IP address and port
         InetAddress address = InetAddress.getByName("224.2.2.3");
         outPacket = new DatagramPacket(outBuf, outBuf.length, address, PORT);
- 
         socket.send(outPacket);
- 
-        System.out.println("Server sends : " + msg);
+
+		//Recebimento da msg
+		inPacket = new DatagramPacket(inBuf, inBuf.length);
+		socket.receive(inPacket);
+        receivemsg = new String(inBuf, 0, inPacket.getLength());
+        System.out.println("From " + inPacket.getAddress() + " Msg : " + receivemsg);
+
         try {
-          Thread.sleep(500);
+			Thread.sleep(500);
         } catch (InterruptedException ie) {
         }
       }
