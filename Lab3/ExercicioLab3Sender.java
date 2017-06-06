ee47672345a303b31493caf99305636de5e49ef1
Gustavo Silva <xgustavossilva@gmail.com>
2017-06-05 19:40:43 -0700

    Algumas correções no LAB3

--- Lab3/ExercicioLab3Sender.java
+++ Lab3/ExercicioLab3Sender.java
@@ -2,49 +2,44 @@ import java.io.*;
 import java.net.*;
 import java.util.Scanner;
 
-/**
- * @author lycog
- */
 public class ExercicioLab3Sender {
-  public static void main(String[] args) {
-    MulticastSocket socket = null;
-    DatagramPacket outPacket = null;
-    DatagramPacket inPacket = null;
-	byte[] inBuf = new byte[256];
-    byte[] outBuf;
-    final int PORT = 8888;
-    Scanner leitura = new Scanner(System.in);
+	public static void main(String[] args) {
+		MulticastSocket socket = null;
+		DatagramPacket outPacket = null;
+		DatagramPacket inPacket = null;
+		byte[] inBuf = new byte[256];
+		byte[] outBuf;
+		final int SENDPORT = 8888;
+		Scanner leitura = new Scanner(System.in);
  
-    try {	
-      socket = new MulticastSocket(8887);
-      String msg;
-	  String receivemsg;
-      InetAddress addressRec = InetAddress.getByName("224.2.2.4");
-	  socket.joinGroup(addressRec);
+    	try {	
+			socket = new MulticastSocket(8887);
+			InetAddress address = InetAddress.getByName("224.2.2.3");
+			socket.joinGroup(address);
+			String msg;
+			String receivemsg;
  
-      while (true) {
+			while (true) {
+				//Envio de msg
+			  	System.out.print("Digite sua mensagem: ");
+			  	msg = leitura.nextLine();
+				outBuf = msg.getBytes();
+				outPacket = new DatagramPacket(outBuf, outBuf.length, address, SENDPORT);
+				socket.send(outPacket);
+				//Recebimento da msg
+				inPacket = new DatagramPacket(inBuf, inBuf.length);
+				socket.receive(inPacket);
+				receivemsg = new String(inBuf, 0, inPacket.getLength());
+				System.out.println("De " + inPacket.getAddress() + " Msg : " + receivemsg);
 
-		//Envio de msg
-      	System.out.print("Digite sua mensagem: ");
-      	msg = leitura.nextLine();
-        outBuf = msg.getBytes();
-        InetAddress address = InetAddress.getByName("224.2.2.3");
-        outPacket = new DatagramPacket(outBuf, outBuf.length, address, PORT);
-        socket.send(outPacket);
+				try {
+					Thread.sleep(500);
+				}catch (InterruptedException ie) {
 
-		//Recebimento da msg
-		inPacket = new DatagramPacket(inBuf, inBuf.length);
-		socket.receive(inPacket);
-        receivemsg = new String(inBuf, 0, inPacket.getLength());
-        System.out.println("From " + inPacket.getAddress() + " Msg : " + receivemsg);
-
-        try {
-			Thread.sleep(500);
-        } catch (InterruptedException ie) {
-        }
-      }
-    } catch (IOException ioe) {
-      System.out.println(ioe);
-    }
-  }
+		    	}
+			}
+		}catch (IOException ioe) {
+			System.out.println(ioe);
+    	}
+  	}
 }
