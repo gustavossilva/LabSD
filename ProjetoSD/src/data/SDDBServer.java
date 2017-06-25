37cb8341d59ede8026c50649774e6d4f264d162f
Luiz Gustavo Gomes Borges <luizg95@ufu.br>
2017-06-25 18:14:17 -0300

    Moved creation of server's parts to its respective thread.

--- ProjetoSD/src/data/SDDBServer.java
+++ ProjetoSD/src/data/SDDBServer.java
@@ -6,6 +6,7 @@ import org.apache.thrift.server.TThreadPoolServer;
 import org.apache.thrift.transport.TServerSocket;
 import org.apache.thrift.transport.TServerTransport;
 import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.transport.TTransportException;
 
 import java.util.ArrayList;
 
@@ -13,30 +14,31 @@ import java.util.ArrayList;
  * Created by gustavovm on 5/21/17.
  */
 public class SDDBServer {
-    private static int port = 9080;
+    final static int BASE_PORT = 9080;
 
     public static void main(String [] args){
         final int N_SERVERS = (args.length > 1) ? Integer.parseInt(args[1]) : 3;
-        Operations.Processor processor;
-        TServerTransport transport;
 
-        try{
+        for(int i =0;i<N_SERVERS;i++){
+            final int id = i;
 
-            for(int i =0;i<N_SERVERS;i++){
-                processor = new Operations.Processor(new SDDBHandler());
-                transport = new TServerSocket(port++);
-                final TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(transport).processor(processor));
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+//                    System.out.println(Thread.currentThread().getName() + ": server " + id);
 
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        System.out.println(Thread.currentThread().getName() + ": " + server);
-                        server.serve();
+                    try {
+                        Operations.Processor processor = new Operations.Processor(new SDDBHandler(id, N_SERVERS));
+                        TServerTransport transport = new TServerSocket(BASE_PORT + id);
+                        new TThreadPoolServer(new TThreadPoolServer.Args(transport).processor(processor)).serve();
                     }
-                }).start();
-            }
-        } catch (Exception x){
-            x.printStackTrace();
+
+                    catch (TTransportException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }).start();
         }
     }
 }
