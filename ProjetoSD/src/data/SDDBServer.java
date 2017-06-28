cba872b3b08218ad234bb6b09442ccef4aba0508
Luiz Gustavo Gomes Borges <luizg95@ufu.br>
2017-06-28 09:19:43 -0300

    Switched to using one process per server.

--- ProjetoSD/src/data/SDDBServer.java
+++ ProjetoSD/src/data/SDDBServer.java
@@ -8,6 +8,7 @@ import org.apache.thrift.transport.TServerTransport;
 import org.apache.thrift.transport.TTransport;
 import org.apache.thrift.transport.TTransportException;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -17,27 +18,30 @@ public class SDDBServer {
     final static int BASE_PORT = 9080;
 
     public static void main(String [] args){
-        final int N_SERVERS = (args.length > 1) ? Integer.parseInt(args[1]) : 3;
+        final int N_SERVERS = (args.length > 0) ? Integer.parseInt(args[0]) : 3;
 
-        for(int i =0;i<N_SERVERS;i++){
-            final int id = i;
+        if (args.length > 1) {
+            final int ID = Integer.parseInt(args[1]);
 
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-//                    System.out.println(Thread.currentThread().getName() + ": server " + id);
+            try (SDDBHandler handler = new SDDBHandler(ID, N_SERVERS)) {
+                Operations.Processor processor = new Operations.Processor(handler);
+                TServerTransport transport = new TServerSocket(BASE_PORT + ID);
+                new TThreadPoolServer(new TThreadPoolServer.Args(transport).processor(processor)).serve();
+            }
 
-                    try (SDDBHandler handler = new SDDBHandler(id, N_SERVERS)) {
-                        Operations.Processor processor = new Operations.Processor(handler);
-                        TServerTransport transport = new TServerSocket(BASE_PORT + id);
-                        new TThreadPoolServer(new TThreadPoolServer.Args(transport).processor(processor)).serve();
-                    }
-
-                    catch (TTransportException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }).start();
+            catch (TTransportException e) {
+                e.printStackTrace();
+            }
         }
+
+        else
+            for (int i = 0; i < N_SERVERS; i++) {
+                try {
+                    new ProcessBuilder("java", "-jar", "Server.jar",
+                                        Integer.toString(N_SERVERS), Integer.toString(i)).start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
     }
 }
