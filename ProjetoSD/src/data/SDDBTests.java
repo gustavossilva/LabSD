57867874ab364f6cfd214b936cf2310982493a51
Luiz Gustavo Gomes Borges <luizg95@ufu.br>
2017-07-30 21:39:15 -0300

    Removed unused code.

--- ProjetoSD/src/data/SDDBTests.java
+++ ProjetoSD/src/data/SDDBTests.java
@@ -19,29 +19,6 @@ public final class SDDBTests {
             Operations.Client client = new Operations.Client(new TBinaryProtocol(transport));
 
             try {
-                assert client.criarVertice(1, 0, "A", 0);
-                assert client.criarVertice(2, 0, "B", 0);
-                assert client.criarVertice(3, 0, "C", 0);
-                assert client.criarVertice(4, 0, "D", 0);
-                assert client.criarVertice(5, 0, "E", 0);
-
-                assert client.criarAresta(1, 2, 1, false, "A -> B");
-                assert client.criarAresta(2, 3, 2, false, "B -> C");
-                assert client.criarAresta(3, 4, 4, false, "C -> D");
-                assert client.criarAresta(4, 5, 8, false, "D -> E");
-            }
-
-            catch (AssertionError e) { System.err.println(e); }
-
-            System.out.println( client.exibirVertice(false) );
-            System.out.println( client.exibirAresta(false) );
-
-            System.out.println("Menor caminho entre A e B: " + client.menorCaminho(1, 2));
-            System.out.println("Menor caminho entre A e C: " + client.menorCaminho(1, 3));
-            System.out.println("Menor caminho entre A e D: " + client.menorCaminho(1, 4));
-            System.out.println("Menor caminho entre A e E: " + client.menorCaminho(1, 5));
-
-            try {
                 assert client.criarVertice(0, 0, "A", 0) : "Vértice A";   // S1
                 assert client.criarVertice(1, 0, "C", 0) : "Vértice C";   // S2
                 assert client.criarVertice(2, 0, "E", 0) : "Vértice E";   // S3
