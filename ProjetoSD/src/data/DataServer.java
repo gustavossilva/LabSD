008b1f11c69af2e826c64225bdc6c58d468676ae
Gustavo Silva <xgustavossilva@gmail.com>
2017-07-30 18:14:49 -0700

    DataServer created

--- ProjetoSD/src/data/DataServer.java
+++ ProjetoSD/src/data/DataServer.java
@@ -1,8 +1,14 @@
 package data;
 
 import io.atomix.catalyst.transport.Address;
+import io.atomix.catalyst.transport.local.LocalServerRegistry;
+import io.atomix.catalyst.transport.local.LocalTransport;
+import io.atomix.catalyst.transport.netty.NettyTransport;
 import io.atomix.copycat.server.CopycatServer;
-import io.atomix.copycat.server.StateMachine;
+import io.atomix.copycat.server.storage.Storage;
+import io.atomix.copycat.server.storage.StorageLevel;
+
+import java.io.File;
 
 /**
  * Created by gustavovm on 7/30/17.
@@ -14,9 +20,19 @@ public class DataServer {
         address = new Address(ip,port);
     }
 
-    public boolean initDServer(){
+    public boolean initDServer(int ThreadNum, String fileDir){
         CopycatServer.Builder builder = CopycatServer.builder(address);
-        builder.withStateMachine(sm::new);
+        builder.withStateMachine(SDDBStateMachine::new);
+        builder.withTransport(NettyTransport.builder().withThreads(ThreadNum).build());
+        builder.withStorage(Storage.builder().withDirectory(new File(fileDir)).withStorageLevel(StorageLevel.DISK).build());
+
+        try{
+            CopycatServer server = builder.build();
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
     }
 
 }
