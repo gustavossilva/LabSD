a6fcb1eea787ddf89a0041c79e7ac1dc94423c3f
Luiz Gustavo Gomes Borges <luizg95@ufu.br>
2017-07-30 21:31:57 -0300

    Uncommented insertions.

--- ProjetoSD/src/data/SDDBTests.java
+++ ProjetoSD/src/data/SDDBTests.java
@@ -3,10 +3,8 @@ package data;
 import models.Operations;
 import org.apache.thrift.TException;
 import org.apache.thrift.protocol.TBinaryProtocol;
-import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
-import org.apache.thrift.transport.TTransportException;
 
 /**
  * Created by luiz on 26/06/17.
@@ -15,12 +13,12 @@ public final class SDDBTests {
     private SDDBTests() {}
 
     public static void main(String[] args) {
-        try (TTransport transport = new TSocket("localhost",9082)) {
+        try (TTransport transport = new TSocket("localhost",9080)) {
             transport.open();
 
             Operations.Client client = new Operations.Client(new TBinaryProtocol(transport));
 
-            /*try {
+            try {
                 assert client.criarVertice(1, 0, "A", 0);
                 assert client.criarVertice(2, 0, "B", 0);
                 assert client.criarVertice(3, 0, "C", 0);
@@ -33,7 +31,7 @@ public final class SDDBTests {
                 assert client.criarAresta(4, 5, 8, false, "D -> E");
             }
 
-            catch (AssertionError e) {}
+            catch (AssertionError e) { System.err.println(e); }
 
             System.out.println( client.exibirVertice(false) );
             System.out.println( client.exibirAresta(false) );
@@ -41,7 +39,7 @@ public final class SDDBTests {
             System.out.println("Menor caminho entre A e B: " + client.menorCaminho(1, 2));
             System.out.println("Menor caminho entre A e C: " + client.menorCaminho(1, 3));
             System.out.println("Menor caminho entre A e D: " + client.menorCaminho(1, 4));
-            System.out.println("Menor caminho entre A e E: " + client.menorCaminho(1, 5));*/
+            System.out.println("Menor caminho entre A e E: " + client.menorCaminho(1, 5));
 
             try {
                 assert client.criarVertice(0, 0, "A", 0) : "Vértice A";   // S1
