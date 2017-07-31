09278709a7b2c69b06b33f3f0519183c31ddd99b
Gustavo Silva <xgustavossilva@gmail.com>
2017-07-30 18:31:50 -0700

    SDDServer now createa DataServer

--- ProjetoSD/src/data/SDDBServer.java
+++ ProjetoSD/src/data/SDDBServer.java
@@ -20,12 +20,15 @@ import java.util.Collection;
  */
 public class SDDBServer {
     final static int BASE_PORT = 9080;
+    final static int BASE_DATA_PORT = 25000;
     public static void main(String [] args){
+        DataServer data;
         final int N_SERVERS = (args.length > 0) ? Integer.parseInt(args[0]) : 3;
 
         if (args.length > 1) {
             final int ID = Integer.parseInt(args[1]);
-
+            data = new DataServer("localhost",BASE_DATA_PORT);
+            data.initDServer(1,"logs");
             try (SDDBHandler handler = new SDDBHandler(ID, N_SERVERS)) {
                 Operations.Processor processor = new Operations.Processor(handler);
                 TServerTransport transport = new TServerSocket(BASE_PORT + ID);
