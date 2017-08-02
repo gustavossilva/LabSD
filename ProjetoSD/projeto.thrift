506d7378f9618f1ed0458720fd5f9675959e4569
Luiz Gustavo Gomes Borges <luizg95@ufu.br>
2017-08-02 01:45:41 -0300

    Fixed methods with flag,

--- ProjetoSD/projeto.thrift
+++ ProjetoSD/projeto.thrift
@@ -17,8 +17,8 @@ struct Aresta {
 }
 
 service Operations {
-    list<string> consultaCidade(1:string cidade),
-    list<string> conhecidosPessoas(1:list<string> nome,2:i32 afinidade),
+    list<string> consultaCidade(1:string cidade, 2: bool first),
+    list<string> conhecidosPessoas(1:list<string> nome,2:i32 afinidade, 3: bool first),
     void carregaGrafo(1:string caminho),
     void salvaGrafo(1:string caminho),
     bool criarVertice(1:i32 nome,2:i32 cor,3:string descricao,4:double peso),
@@ -34,6 +34,6 @@ service Operations {
     string exibirAresta(1:bool flag),
     list<Vertice> listarVerticesArestas(1:i32 v1,2:i32 v2),
     list<Aresta> listarArestasVertice(1:i32 nomeV,2:bool flag),
-    list<Vertice> listarVizinhosVertice(1:i32 nomeV, 2:bool first),
-    string menorCaminho(1:i32 v1,2:i32 v2, 3:bool first)
+    list<Vertice> listarVizinhosVertice(1:i32 nomeV),
+    string menorCaminho(1:i32 v1,2:i32 v2)
 }
